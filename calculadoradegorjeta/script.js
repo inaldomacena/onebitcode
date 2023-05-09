e4367b34321389e0867f66c8d0517fac38b43de8
Inaldo Macena <inaldomacena19@gmail.com>
2023-05-09 02:28:52 -0300

    atualização

---
+++ calculadoradegorjeta/script.js
@@ -0,0 +1,10 @@
+
+
+function calculateTip(event){
+
+}
+
+document.getElementById("totalTip").style.display = "none";
+document.getElementById("each").style.display = "none";
+
+document.getElementById('tipsForm').addEventListener('submit'.calculateTip);
